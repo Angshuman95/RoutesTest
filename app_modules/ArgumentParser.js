9c90ec7fa60f1fa2f4362f338b701ff895b889a3
Angshuman <ghosh.angshuman1@gmail.com>
2020-08-12 16:58:56 +0530

    Test done

--- app_modules/ArgumentParser.js
+++ app_modules/ArgumentParser.js
@@ -4,15 +4,18 @@ module.exports = { parse };
 function parseArgs(inputArgsArr)
 {
     if(inputArgsArr.length === 0) return ErrorEnum.NoInput;
-
+    
     let inputOptions = {};
 
     for(let i = 0; i < inputArgsArr.length; ++i)
     {
-        if(inputArgsArr[i] === '-p' || inputArgsArr[i] === '-P' || inputArgsArr[i] === '--path')
-            inputArgsArr.configFilePath = inputArgsArr[++i];
+        if(inputArgsArr[i] === '-p'){
+            if(inputArgsArr[++i] === undefined || inputArgsArr[i] === '-o' || inputArgsArr[i] === '-p') continue;
+            inputOptions.configFilePath = inputArgsArr[i];
+            continue;
+        }
 
-        if(inputArgsArr[i] === '-o' || inputArgsArr[i] === '-O' || inputArgsArr[i] === '--option')
+        if(inputArgsArr[i] === '-o')
             inputOptions.optionName = inputArgsArr[++i];
     }
 
@@ -37,9 +40,9 @@ function parse(inputArgs)
     this.inputOptions = parseArgs(this.inputArgs);
 
     if(this.inputOptions == ErrorEnum.NoInput)
-        return 'Error: Expected input in the form \nnode main.js -p path [-o option]';
+        return 'Error: Expected input in the form:\nnode main.js -p path [-o option]';
     else if(this.inputOptions == ErrorEnum.NoConfigFilePath)
-        return 'Error: It is mandatory to provide the path of config file';
+        return 'Error: It is mandatory to provide the path of config file.';
     else
         return this.inputOptions;
 }
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 // Collect the command-line arguments
 
 let inputArgs = process.argv.slice(2);
+
+let inputOption = require('./app_modules/ArgumentParser').parse(inputArgs);
+
+if(typeof(inputOption) === 'string')
+{
+    console.log(inputOption + '\n\nAborting application.');
+    process.exit();
+}
+
